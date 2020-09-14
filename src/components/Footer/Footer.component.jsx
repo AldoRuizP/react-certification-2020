78f0998353e41896b45b03155da61666a2b87614
Aldo <aldo.ruiz@wizeline.com>
2020-09-14 13:05:42 -0500

    add footer component

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -2,10 +2,50 @@ import React from 'react';
 
 import './Footer.styles.css';
 
-const { render } = require('@testing-library/react');
-
 function Footer() {
-  render(<div>Hello</div>);
+  return (
+    <div className="footer">
+      <p>React Certification by Aldo Ruiz</p>
+      <p>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/freepik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {' '}
+          Freepik
+        </a>
+        from
+        <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">
+          {' '}
+          www.flaticon.com
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://iconscout.com/icon-pack/animal-flat-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {' '}
+          Animal Flat Colors Icon Pack
+        </a>
+        by
+        <a
+          href="https://iconscout.com/contributors/kerismaker"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kerismaker
+        </a>{' '}
+        on
+        <a href="https://iconscout.com" target="_blank" rel="noopener noreferrer">
+          Iconscout
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -8,30 +8,7 @@ function Layout({ children }) {
     <>
       <NavBar />
       <main className="container">{children}</main>
-      <Footer>
-        <p>
-          Icons made by{' '}
-          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-            Freepik
-          </a>{' '}
-          from{' '}
-          <a href="https://www.flaticon.com/" title="Flaticon">
-            {' '}
-            www.flaticon.com
-          </a>
-        </p>
-        <p>
-          <a
-            href="https://iconscout.com/icon-pack/animal-flat-colors"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Animal Flat Colors Icon Pack
-          </a>{' '}
-          by <a href="https://iconscout.com/contributors/kerismaker">Kerismaker</a> on{' '}
-          <a href="https://iconscout.com">Iconscout</a>
-        </p>
-      </Footer>
+      <Footer />
     </>
   );
 }
