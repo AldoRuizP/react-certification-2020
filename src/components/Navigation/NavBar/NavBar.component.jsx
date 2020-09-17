a4dfe490f298203b43f66b11d06484db0db20841
Aldo <aldo.ruiz@wizeline.com>
2020-09-17 10:44:00 -0500

    Add navbar header

--- src/components/Navigation/NavBar/NavBar.component.jsx
+++ src/components/Navigation/NavBar/NavBar.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavItem from '../NavItem';
 import DropdownMenu from '../DropdownMenu';
 import SearchBar from '../SearchBar';
-import { Search } from '../../Icons/navigation';
 
+import { Search, WizelineLogo, WizelineHeaderLight } from '../../Icons/navigation';
 import { Snake } from '../../Icons/avatars';
 
 import './NavBar.styles.css';
@@ -12,19 +13,22 @@ const currentAvatar = <Snake />;
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-title">
-        <h1>WizeTube</h1>
-      </div>
-      <ul className="navbar-nav">
-        <NavItem icon={<Search />}>
-          <SearchBar />
-        </NavItem>
-        <NavItem icon={currentAvatar}>
-          <DropdownMenu />
-        </NavItem>
-      </ul>
-    </nav>
+    <Link to="/">
+      <nav className="navbar">
+        <div className="navbar-title">
+          <WizelineLogo className="wizeline-logo" />
+          <WizelineHeaderLight className="wizeline-header-light" />
+        </div>
+        <ul className="navbar-nav">
+          <NavItem icon={<Search />}>
+            <SearchBar />
+          </NavItem>
+          <NavItem icon={currentAvatar}>
+            <DropdownMenu />
+          </NavItem>
+        </ul>
+      </nav>
+    </Link>
   );
 }
 
