1dd66f19029eb86e02d3846d9b3f758963024a81
Aldo <aldo.ruiz@wizeline.com>
2020-09-18 11:25:30 -0500

    Migrate stylsheet to styled component. Add animation to searc bar

--- src/components/Navigation/SearchBar/SearchBar.component.jsx
+++ src/components/Navigation/SearchBar/SearchBar.component.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-import './SearchBar.styles.css';
+const extend = keyframes`
+  from {
+    transform: scale(0, 1);
+  }
+
+  to {
+    transform: scale(1, 1);
+  } 
+`;
+
+const Input = styled.input`
+  border-radius: inherit;
+  height: 40px;
+  width: 25rem;
+  outline: none;
+  border: none;
+  position: absolute;
+  right: 4.2rem;
+  border-radius: 20px;
+  box-shadow: 0px 0px 0px 1px white inset;
+  text-indent: 10px;
+  background-color: var(--bg-accent);
+  transform-origin: 100% 50%;
+  animation: ${extend} 500ms ease;
+`;
 
 function SearchBar() {
   return (
-    <div className="search-bar">
-      <input
-        className="search-bar-input"
-        type="text"
-        id="searchItem"
-        placeholder="Search for a video..."
-        autoComplete="off"
-      />
-    </div>
+    <Input
+      type="text"
+      id="searchItem"
+      placeholder="Search for a video..."
+      autoComplete="off"
+    />
   );
 }
 
