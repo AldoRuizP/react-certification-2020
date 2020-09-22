5abda57823965af0f25ce7d76777bee36268d954
Aldo <aldo.ruiz@wizeline.com>
2020-09-22 12:03:31 -0500

    clear homepage

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,32 +1,17 @@
 import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Link, useHistory } from 'react-router-dom';
 
 import VideoList from '../../components/Video/List';
 import useVideoFeed from '../../utils/hooks/useVideoFeed';
-import { useAuth } from '../../providers/Auth';
 
 const Section = styled.section`
   text-align: center;
 `;
-const Title = styled.h1`
-  font-size: 3rem;
-  letter-spacing: -2px;
-`;
-const Subtitle = styled.h2``;
 
 function HomePage() {
-  const history = useHistory();
   const [videoList, setVideoList] = useState([]);
   const { videos } = useVideoFeed();
   const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   useEffect(() => {
     setVideoList(videos);
@@ -36,20 +21,6 @@ function HomePage() {
     <>
       <Section ref={sectionRef}>
         <VideoList videos={videoList} />
-        <Title>Hello stranger!</Title>
-        {authenticated ? (
-          <>
-            <Subtitle>Good to have you back</Subtitle>
-            <span>
-              <Link to="/" onClick={deAuthenticate}>
-                ← logout
-              </Link>
-              <Link to="/secret"> show me something cool →</Link>
-            </span>
-          </>
-        ) : (
-          <Link to="/login">Let me in →</Link>
-        )}
       </Section>
     </>
   );
