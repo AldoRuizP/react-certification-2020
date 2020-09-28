61940f122264a15bd672ab4585a5ec798a5b4f24
Aldo <aldo.ruiz@wizeline.com>
2020-09-28 09:52:57 -0500

    Update tests

--- src/utils/tests/storage.spec.js
+++ src/utils/tests/storage.spec.js
@@ -9,4 +9,18 @@ describe('Test the storage function', () => {
     const res = storage.get('SOME_KEY');
     expect(res).toBe('SOME_VALUE');
   });
+
+  it('Should not get a key value', () => {
+    const res = storage.get('SOME_INVALID_KEY');
+    expect(res).toBeNull();
+  });
+
+  it('Should throw an error', () => {
+    try {
+      storage.set('INVALID_OBJECT', undefined);
+      storage.get('INVALID_OBJECT');
+    } catch (e) {
+      expect(e.message).toBe('Error parsing storage item "INVALID_OBJECT".');
+    }
+  });
 });
